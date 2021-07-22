8f920550dfe03cf8c5cfa94e366df454fcb18f29
yarayacqb <yaraya_cqb@hotmail.com>
2021-07-22 15:20:01 -0400

    E

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Listar todo 
+  # get "tasks", to: "tasks#index"
+  # # Listar uno
+  # get "tasks/:id", to: "tasks#show", as: "task"
+  # # CREAR TASK
+  # get "tasks/new", to: "tasks#new", as: "new_task" # renderizar
+  # post "tasks", to: "tasks#create" # crear
+  # # UPDATE A TASK
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit_task" # mostrar
+  # patch "tasks/:id", to: "tasks#update" # procesar
+  # #  DELETE A TASK
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
---
+++ db/migrate/20210721203624_tasks_controller.rb
@@ -0,0 +1,5 @@
+class TasksController < ActiveRecord::Migration[6.0]
+  
+
+
+end
